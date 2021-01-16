e9871425da014e44708521e509dd4b4108a2d4d8
Barbara Ferreira <bbf@charlietango.dk>
2021-01-16 20:08:57 +0100

    Ex1 - EC4 - Solution

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,25 +3,36 @@
 
 import * as React from 'react'
 
-const countReducer = (state, newState) => typeof newState === 'function' ? newState(state) : newState
+/*const countReducer = (state, newState) => typeof newState === 'function' ? newState(state) : newState*/
+
+
+const countReducer = (state, newState) => {
+  const { type, step } = newState
+  switch(type) {
+    case 'INCREMENT':
+      return {...state, count: state.count + step}
+    default:
+      return state
+  }
+}
 
 function Counter({initialCount = 0, step = 1}) {
 /*Extra credit 2
 See if you can figure out how to make your reducer support both the
 object as in the last extra credit as well as a function callback:
- const [state, setState] = React.useReducer(countReducer, {
-  count: initialCount,
-})
- const {count} = state
- const increment = () => setState({count: count + step})
-* */
-
-  const [state, setState] = React.useReducer(countReducer, {
+  const [state, dispatch] = React.useReducer(countReducer, {
     count: initialCount,
   })
   const {count} = state
-  const increment = () =>
-    setState(currentState => ({count: currentState.count + step}))
+  const increment = () => dispatch({type: 'INCREMENT', step})
+* */
+
+  const [state, dispatch] = React.useReducer(countReducer, {
+    count: initialCount,
+  })
+  const {count} = state
+  /*console.log('count', count)*/
+  const increment = () => dispatch({type: 'INCREMENT', step})
 
   return <button onClick={increment}>{count}</button>
 }
